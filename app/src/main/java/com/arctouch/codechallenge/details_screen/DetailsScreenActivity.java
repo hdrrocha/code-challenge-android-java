3541403306d2a5c1bde7faed4d7f98ba25f4630d
Heder Rocha <hedie.strokes@gmail.com>
2018-07-13 00:04:46 -0300

    details screan create

---
+++ app/src/main/java/com/arctouch/codechallenge/details_screen/DetailsScreenActivity.java
@@ -0,0 +1,15 @@
+package com.arctouch.codechallenge.details_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arctouch.codechallenge.R;
+
+public class DetailsScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_screen);
+    }
+}
