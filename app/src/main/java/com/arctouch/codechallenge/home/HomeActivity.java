7bed99e59894b75c332c2c9124b946c5ff5cec10
Heder Rocha <hedie.strokes@gmail.com>
2018-07-13 00:04:46 -0300

    creating movie detail screen

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
@@ -1,19 +1,27 @@
 package com.arctouch.codechallenge.home;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.arctouch.codechallenge.R;
 import com.arctouch.codechallenge.api.TmdbApi;
 import com.arctouch.codechallenge.base.BaseActivity;
 import com.arctouch.codechallenge.data.Cache;
+import com.arctouch.codechallenge.details_screen.DetailsScreenActivity;
 import com.arctouch.codechallenge.model.Genre;
 import com.arctouch.codechallenge.model.Movie;
+import com.arctouch.codechallenge.util.RecyclerItemClickListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
@@ -22,7 +30,7 @@ public class HomeActivity extends BaseActivity {
 
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
-
+    private List<Movie> listMovie = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,7 @@ public class HomeActivity extends BaseActivity {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(response -> {
+                    listMovie.addAll(response.results);
                     for (Movie movie : response.results) {
                         movie.genres = new ArrayList<>();
                         for (Genre genre : Cache.getGenres()) {
@@ -47,5 +56,32 @@ public class HomeActivity extends BaseActivity {
                     recyclerView.setAdapter(new HomeAdapter(response.results));
                     progressBar.setVisibility(View.GONE);
                 });
+        this.recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        this.recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Movie movie = listMovie.get(position);
+                                Toast.makeText(getApplicationContext(), "item precionado" + movie.title.toString(), Toast.LENGTH_LONG).show();
+                                Context context = view.getContext();
+                                Intent intent = new Intent(context, DetailsScreenActivity.class);
+                                context.startActivity(intent);
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+
+        );
     }
 }
