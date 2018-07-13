43a5746c9369cff88ab205c9cb7ce449e566db94
Helder Rocha <hedie.strokes@gmail.com>
2018-07-13 12:10:32 -0300

    consuming movie details

--- app/src/main/java/com/arctouch/codechallenge/details_screen/DetailsScreenActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/details_screen/DetailsScreenActivity.java
@@ -1,13 +1,30 @@
 package com.arctouch.codechallenge.details_screen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.arctouch.codechallenge.R;
+import com.arctouch.codechallenge.api.TmdbApi;
 import com.arctouch.codechallenge.base.BaseActivity;
+import com.arctouch.codechallenge.data.Cache;
+import com.arctouch.codechallenge.model.Genre;
 import com.arctouch.codechallenge.model.Movie;
+import com.arctouch.codechallenge.util.MovieImageUrlBuilder;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class DetailsScreenActivity extends BaseActivity {
 
@@ -17,6 +34,15 @@ public class DetailsScreenActivity extends BaseActivity {
     private TextView textViewYear;
     private TextView textViewLikes;
     private TextView textViewOverview;
+    private final MovieImageUrlBuilder movieImageUrlBuilder = new MovieImageUrlBuilder();
+    TmdbApi api = new Retrofit.Builder()
+            .baseUrl(TmdbApi.URL)
+            .client(new OkHttpClient.Builder().build())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(TmdbApi.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +56,40 @@ public class DetailsScreenActivity extends BaseActivity {
         textViewOverview = (TextView) findViewById(R.id.textViewOverview);
 
         Bundle data = getIntent().getExtras();
+        String movieID =  data.getString("serialize_data");
 
-        Movie movie = (Movie) data.getSerializable("movie_selected");
-        textViewTitle.setText(movie.title);
+        api.movie(Long.valueOf(movieID),TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> {
+                    Cache.setGenres(response.genres);
+
+                    this.textViewTitle.setText(response.title);
+                    this.textViewYear.setText(response.releaseDate);
+                    this.textViewLikes.setText(TextUtils.join(", ", response.genres));
+//                    Movie movie = response;
+//                        movie.genres = new ArrayList<>();
+//                        for (Genre genre : Cache.getGenres()) {
+//                            if (movie.genreIds.contains(genre.id)) {
+//                                movie.genres.add(genre);
+//                            }
+//                        }
+
+                    this.textViewOverview.setText(response.overview);
+                    String posterPath = response.posterPath;
+                    if (TextUtils.isEmpty(posterPath) == false) {
+                        Glide.with(getBaseContext())
+                                .load(movieImageUrlBuilder.buildPosterUrl(posterPath))
+                                .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                                .into(imageMovie);
+                    }
+                    String backdropPath = response.backdropPath;
+                    if (TextUtils.isEmpty(backdropPath) == false) {
+                        Glide.with(getBaseContext())
+                                .load(movieImageUrlBuilder.buildPosterUrl(posterPath))
+                                .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                                .into(imageMovieBackground);
+                    }
+                });
     }
 }
